57a40294d3dc0f9e4ca3286ef661b511a0a75b76
Soraku7 <2673140287@qq.com>
2023-08-23 13:58:14 +0800

    Init

---
+++ Source/UEShootGame/UEShootGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEShootGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UESHOOTGAME_API AUEShootGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
